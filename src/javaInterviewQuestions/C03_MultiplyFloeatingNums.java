edc10dbda5c49cfc9e97fcba688508be85b8d64d
Cagatay <cagataybayramgonen@gmail.com>
2021-11-20 16:32:27 +0300

    C03

---
+++ src/javaInterviewQuestions/C03_MultiplyFloeatingNums.java
@@ -0,0 +1,10 @@
+package javaInterviewQuestions;
+
+public class C03_MultiplyFloeatingNums {
+    public static void main(String[] args) {
+        //Question: Multiply two floating numbers
+        float a = 5.3f;
+        float b = 4.7f;
+        System.out.println("the result is : " + (a*b));
+    }
+}
