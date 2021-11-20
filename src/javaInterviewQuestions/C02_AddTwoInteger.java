fea68e53e4db0940032c85260a5332c818563bb0
Cagatay <cagataybayramgonen@gmail.com>
2021-11-20 16:16:53 +0300

    C02

---
+++ src/javaInterviewQuestions/C02_AddTwoInteger.java
@@ -0,0 +1,11 @@
+package javaInterviewQuestions;
+
+public class C02_AddTwoInteger {
+    public static void main(String[] args) {
+        //Question: Add two integer
+        int a = 23;
+        int b = 34;
+        int sum = a + b;
+        System.out.println("a + b = " + sum);
+    }
+}
