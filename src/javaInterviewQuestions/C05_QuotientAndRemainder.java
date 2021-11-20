787e670779be3101e6f1def6c6e37d1e488aef4a
Cagatay <cagataybayramgonen@gmail.com>
2021-11-20 16:40:41 +0300

    C05

---
+++ src/javaInterviewQuestions/C05_QuotientAndRemainder.java
@@ -0,0 +1,14 @@
+package javaInterviewQuestions;
+
+public class C05_QuotientAndRemainder {
+    public static void main(String[] args) {
+        int dividend = 30; //bolunen
+        int division = 4; //bolen
+
+        int quotient = dividend / division; //bolum
+        int remainder = dividend & division; //kalan
+
+        System.out.println(quotient);
+        System.out.println(remainder);
+    }
+}
