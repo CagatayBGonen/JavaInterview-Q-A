d657dae746786611131a4ecbc53a124ae557d770
Cagatay <cagataybayramgonen@gmail.com>
2021-11-20 16:36:55 +0300

    C04

---
+++ src/javaInterviewQuestions/C04_ASCIIValueOfCharacter.java
@@ -0,0 +1,20 @@
+package javaInterviewQuestions;
+
+public class C04_ASCIIValueOfCharacter {
+    public static void main(String[] args) {
+        //Question: Value of ASCII of characters
+        char a = 'a'; // 97
+
+        //1.way
+        int ascii = a;
+
+        //2.way
+        int asciiNum = (int)a;
+
+        System.out.println(ascii);
+        System.out.println(asciiNum);
+    }
+
+
+
+}
